45c91c6ee3e4ec5b4e302bb8278b778d1cab5f17
root <root@virtual1.(none)>
2013-04-26 21:55:46 +0800

    mjlf

--- src/mjlf.c
+++ src/mjlf.c
@@ -1,8 +1,27 @@
 #include <stdlib.h>
 #include "mjlog.h"
 #include "mjlf.h"
+#include "mjsock.h"
 
-mjLF mjLF_New( mjProc Routine, int threadNum )
+static void* mjLF_Routine( void* arg )
+{
+    mjLF server = ( mjLF ) arg;
+
+    while ( 1 ) {
+        int cfd = mjSock_Accept( server->port );
+        if ( cfd < 0 ) continue;
+        // invoke follows
+        mjThreadPool2_AddWork( server->tPool, mjLF_Routine, server );
+
+        if ( server->Routine ) {
+            server->Routine( &cfd );
+        }
+        break;
+    }
+    return NULL; 
+}
+
+mjLF mjLF_New( mjProc Routine, int maxThread, int port )
 {
     mjLF server = ( mjLF ) calloc( 1, sizeof( struct mjLF ) );
     if ( !server ) {
@@ -10,5 +29,26 @@ mjLF mjLF_New( mjProc Routine, int threadNum )
         return NULL;
     }
 
+    server->tPool = mjThreadPool2_New( maxThread );
+    if ( !server->tPool ) {
+        MJLOG_ERR( "mjthreadpool create error" );
+        free( server );
+        return NULL;
+    }
+    server->port    = port;
+    server->Routine = Routine;
+    mjThreadPool2_AddWork( server->tPool, mjLF_Routine, server );
+
     return server;
+}
+
+bool mjLF_Delete( mjLF server )
+{
+    if ( !server ) {
+        MJLOG_ERR( "server is null" );
+        return false;
+    }
+
+    mjThreadPool2_Delete( server->tPool );
+    return true;
 }
--- src/mjlf.h
+++ src/mjlf.h
@@ -1,12 +1,17 @@
 #ifndef __MJLF_H
 #define __MJLF_H
 
+#include <stdbool.h>
 #include "mjthreadpool2.h"
 
 struct mjLF {
     mjThreadPool2   tPool; 
     mjProc          Routine;
+    int             port;
 };
 typedef struct mjLF* mjLF;
+
+extern mjLF mjLF_New( mjProc Routine, int maxThread, int port );
+extern bool mjLF_Delete( mjLF server );
 
 #endif
