bd6cfe476ae98c5306936e845dcbcb097711fff2
wenfang <wangwenfang@gmail.com>
2013-05-20 14:24:45 +0800

    fin

--- test/mjmd5_t.c
+++ test/mjmd5_t.c
@@ -3,7 +3,7 @@
 
 int main() {
     unsigned char digest[16];
-    mjMD5( "23kjsa987*%&623lkjs0UJ243kash%%%kashJJ", digest );
+    mjMD5( "", digest );
     for( int i = 0; i < 16; i++ ) {
         printf( "%X ", digest[i] );
     }
