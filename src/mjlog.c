1828879084c16ecbbd105c35851322badb330991
wenfang <wangwenfang@gmail.com>
2013-08-10 15:30:03 +0800

    mjlog

--- src/mjlog.c
+++ src/mjlog.c
@@ -5,23 +5,20 @@
 
 #define MAX_MSG_LEN 512
 
-void mjlog_init( const char* name )
-{
-    openlog( name, LOG_CONS|LOG_PID, LOG_LOCAL6 );
+void mjlog_init(const char* name) {
+  openlog(name, LOG_CONS|LOG_PID, LOG_LOCAL6);
 }
 
-void mjlog_write( int priority, const char* message, ... )
-{
-    char msg[MAX_MSG_LEN + 1]; 
-    va_list ap; 
+void mjlog_write(int priority, const char* message, ...) {
+  char msg[MAX_MSG_LEN + 1]; 
+  va_list ap; 
     
-    va_start( ap, message );
-    vsnprintf( msg, MAX_MSG_LEN, message, ap );
-    syslog( priority, "%s", msg );
-    va_end( ap );
+  va_start(ap, message);
+  vsnprintf(msg, MAX_MSG_LEN, message, ap);
+  syslog(priority, "%s", msg);
+  va_end(ap);
 }
 
-void mjlog_close()
-{
-    closelog();
+void mjlog_close() {
+  closelog();
 }
--- test/mjtest_t.c
+++ test/mjtest_t.c
@@ -4,6 +4,7 @@
 #include "mjstr.h"
 #include "mjcomm.h"
 #include "mjthread.h"
+#include "mjsql.h"
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
