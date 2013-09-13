0d3f026b8197e1b1b07d4e45f52aa5104bc32bd1
wenfang <wangwenfang@gmail.com>
2013-09-13 15:14:15 +0800

    update mjlockless

--- src/mjlockless.c
+++ src/mjlockless.c
@@ -1,1 +1,65 @@
 #include "mjlockless.h"
+#include "mjlog.h"
+#include <stdlib.h>
+
+/*
+===============================================================================
+mjlockless_push
+===============================================================================
+*/
+void mjlockless_push(mjlockless lockless, void* value) {
+  uint32_t tail = __sync_fetch_and_add(&lockless->tail, 1) % lockless->_size;
+  lockless->_queue[tail] = value;
+}
+
+/*
+===============================================================================
+mjlockless_pop
+===============================================================================
+*/
+void* mjlockless_pop(mjlockless lockless) {
+  if (lockless->head % lockless->_size == lockless->tail % lockless->_size) {
+    return NULL;
+  }
+  uint32_t head = lockless->head;
+  if (!__sync_bool_compare_and_swap(&lockless->head, head, head + 1)) {
+    return NULL;
+  }
+  return lockless->_queue[head % lockless->_size];
+}
+
+/*
+===============================================================================
+mjlockless_new
+  alloc mjlockless struct
+===============================================================================
+*/
+mjlockless mjlockless_new(int size) {
+  if (size <= 0) {
+    MJLOG_ERR("size is not valid");
+    return NULL;
+  }
+  mjlockless lockless = (mjlockless)calloc(1, sizeof(struct mjlockless) + 
+      sizeof(void*) * size);
+  if (!lockless) {
+    MJLOG_ERR("calloc error");
+    return NULL; 
+  }
+  lockless->_size = size;
+  return lockless;
+}
+
+/*
+===============================================================================
+mjlockless_delete
+  delete lockless struct
+===============================================================================
+*/
+bool mjlockless_delete(mjlockless lockless) {
+  if (!lockless) {
+    MJLOG_ERR("lockless is null");
+    return false;
+  }
+  free(lockless);
+  return true;
+}
--- src/mjlockless.h
+++ src/mjlockless.h
@@ -1,14 +1,19 @@
 #ifndef __MJLOCKLESS_H
 #define __MJLOCKLESS_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
 struct mjlockless {
   uint32_t  head;
   uint32_t  tail;
-  int       _length;
+  int       _size;
   void*     _queue[0];
 };
 typedef struct mjlockless* mjlockless;
 
+extern void       mjlockless_push(mjlockless lockless, void* value);
+extern void*      mjlockless_pop(mjlockless lockless);
 extern mjlockless mjlockless_new(int size);
 extern bool       mjlockless_delete(mjlockless lockless);
 
