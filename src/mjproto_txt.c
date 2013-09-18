dff5e7720898824a2f8820d85445349fbf9b06a0
wenfang <wangwenfang@gmail.com>
2013-09-18 16:23:28 +0800

    update txt

--- src/mjproto_txt.c
+++ src/mjproto_txt.c
@@ -3,6 +3,12 @@
 #include "mjlog.h"
 #include "mjlf.h"
 
+/*
+===============================================================================
+mjproto_txt_init
+  init proto txt.
+===============================================================================
+*/
 void* mjproto_txt_init(void* arg) {
 	mjlf server = (mjlf) arg;
 	mjproto_txt_routine_list routine_list = server->srv_init_arg;
@@ -14,7 +20,7 @@ void* mjproto_txt_init(void* arg) {
 
 /*
 ===============================================================================
-mjTxt_RunCmd
+mjproto_txt_run_cmd
   run txt protocol command
 ===============================================================================
 */
@@ -27,6 +33,7 @@ static bool mjproto_txt_run_cmd(mjconnb conn, mjproto_txt_data cmd_data,
     MJLOG_ERR("data create error");
     return false;  
   }
+  // read command
   int ret = mjconnb_readuntil(conn, "\r\n", data);
   if (ret == -2) {
     mjconnb_writes(conn, "+ read timetout\r\n");
@@ -39,21 +46,21 @@ static bool mjproto_txt_run_cmd(mjconnb conn, mjproto_txt_data cmd_data,
     goto failout1;
   }
   // split string
-  mjstrlist str_list = mjstrlist_new();
-  if (!str_list) {
+  mjstrlist s_list = mjstrlist_new();
+  if (!s_list) {
 		mjconnb_writes(conn, "+ inner error\r\n");
     MJLOG_ERR("mjstrlist create error");
     goto failout1;
   }
-  mjstr_split(data, " ", str_list);
-  if (str_list->length < 1) {
+  mjstr_split(data, " ", s_list);
+  if (s_list->length < 1) {
     mjconnb_writes(conn, "+ command error\r\n");
     goto failout2;
   }
-  // get tag and cmd
-  cmd_data->cmd = mjstrlist_get(str_list, 0);
+  // get command
+  cmd_data->cmd = mjstrlist_get(s_list, 0);
   mjstr_strim(cmd_data->cmd);
-  cmd_data->args = str_list;
+  cmd_data->args = s_list;
   cmd_data->conn = conn;
   // run routine
   for (int i = 0; routine_list[i].cmd != NULL; i++) {
@@ -62,19 +69,25 @@ static bool mjproto_txt_run_cmd(mjconnb conn, mjproto_txt_data cmd_data,
     // run routine
     if (routine_list[i].Routine) (*routine_list[i].Routine)(cmd_data);
     // clean and return
-    mjstrlist_delete(str_list);
+    mjstrlist_delete(s_list);
     mjstr_delete(data);
     return true;
   }  
   mjconnb_writes(conn, "+ wrong command\r\n");
-  // release resource
 failout2:
-  mjstrlist_delete(str_list);
+  mjstrlist_delete(s_list);
 failout1:
   mjstr_delete(data);	
   return false;
 }
 
+/*
+===============================================================================
+mjproto_txt_routine
+  run by mjlf. the main routine of mjproto_txt.
+  arg is mjconnb
+===============================================================================
+*/
 void* mjproto_txt_routine(void* arg) {
   // sanity check
 	mjconnb conn = (mjconnb) arg;
@@ -82,13 +95,14 @@ void* mjproto_txt_routine(void* arg) {
     MJLOG_ERR("conn is null");
     return NULL;
   }
-	// get server and routine list
+	// get server
 	mjlf server = mjconnb_get_obj(conn, "server");
 	if (!server) {
 		mjconnb_writes(conn, "+ inner error\r\n");
 		MJLOG_ERR("no server found");
 		return NULL;
 	}
+  // get routine_list
 	mjproto_txt_routine_list routine_list = mjlf_get_obj(server, "routine_list");
   if (!routine_list) {
     mjconnb_writes(conn, "+ no command list\r\n");
@@ -101,11 +115,18 @@ void* mjproto_txt_routine(void* arg) {
 	cmd_data.args = NULL;
 	cmd_data.conn	= NULL;
 	cmd_data.finished = false;
-	// run routine
-	while (mjproto_txt_run_cmd(conn, &cmd_data, routine_list) && !cmd_data.finished);
+	// run routine and loop till finished
+	while (mjproto_txt_run_cmd(conn, &cmd_data, routine_list) && 
+      !cmd_data.finished);
 	return NULL;
 }
 
+/*
+===============================================================================
+mjproto_txt_finished
+  set conn finished
+===============================================================================
+*/
 bool mjproto_txt_finished(mjproto_txt_data cmd_data) {
 	if (!cmd_data) {
 		MJLOG_ERR("cmd_data is null");
