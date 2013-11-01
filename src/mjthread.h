f0ae4331d5032586d079c6d08080255610641e34
wenfang <wangwenfang@gmail.com>
2013-11-01 09:59:07 +0800

    add mjthread cb

--- src/mjthread.h
+++ src/mjthread.h
@@ -14,9 +14,11 @@ struct mjthread {
   struct mjthreadpool*  _tpool;   // threadpool this thread is in
 
   mjProc                _INIT;    // run once when thread init
-  void*                 iarg;     // Init Routine use this
+  void*                 iarg;     // Init Routine arg
   mjProc                _RT;      // routine to be run
-  void*                 arg;      // Routine use this
+  void*                 arg;      // routine arg
+  mjProc                _CB;      // CallBack Routine
+  void*                 cbarg;    // CallBack Routine arg
 
   mjmap                 _map;     // arg map for this thread
 
@@ -26,10 +28,12 @@ struct mjthread {
 };
 typedef struct mjthread* mjthread;
 
-
+// used for MJTHREAD_NORMAL
 extern bool     mjthread_add_task(mjthread thread, mjProc RT, void* arg);
 extern bool     mjthread_run(mjthread thread);
+// used for MJTHREAD_ONCE
 extern bool     mjthread_run_once(mjthread thread, mjProc RT, void* arg);
+
 extern mjthread mjthread_new();
 extern bool     mjthread_delete(mjthread thread);
 
@@ -49,6 +53,13 @@ static inline bool mjthread_set_init(mjthread thread, mjProc INIT, void* iarg) {
   if (!thread) return false;
   thread->_INIT = INIT;
   thread->iarg = iarg;
+  return true;
+}
+
+static inline bool mjthread_set_cb(mjthread thread, mjProc CB, void* cbarg) {
+  if (!thread) return false;
+  thread->_CB = CB;
+  thread->cbarg = cbarg;
   return true;
 }
 
